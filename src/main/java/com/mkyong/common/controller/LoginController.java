5e937fd3ee456e0fe52722f7f420799966c01e21
snowdream <yanghui1986527@gmail.com>
2013-08-08 11:19:56 +0800

    [Update] ok

--- src/main/java/com/mkyong/common/controller/LoginController.java
+++ src/main/java/com/mkyong/common/controller/LoginController.java
@@ -1,44 +1,42 @@
 package com.mkyong.common.controller;
- 
+
 import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+
 @Controller
 public class LoginController {
- 
-	@RequestMapping(value="/welcome", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model, Principal principal ) {
- 
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model, Principal principal) {
+
 		String name = principal.getName();
 		model.addAttribute("username", name);
 		model.addAttribute("message", "Spring Security Custom Form example");
 		return "hello";
- 
+
 	}
- 
-	@RequestMapping(value="/login", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(ModelMap model) {
- 
+
 		return "login";
- 
+
 	}
-	
-	@RequestMapping(value="/loginfailed", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/loginfailed", method = RequestMethod.GET)
 	public String loginerror(ModelMap model) {
- 
+
 		model.addAttribute("error", "true");
 		return "login";
- 
 	}
-	
-	@RequestMapping(value="/logout", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(ModelMap model) {
- 
+
 		return "login";
- 
 	}
-	
+
 }
